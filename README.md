1f57b41076c7bd4b96223f092ad4c40ceadbf991
unknown <matt.mcquain@gmail.com>
2023-04-04 20:11:10 -0700

    fixed the CustomButton component. Starting work on the Customizer page.

---
+++ README.md
@@ -0,0 +1,1 @@
+3D Product Demo using ThreeJS
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,13 +9,61 @@ import {
   slideAnimation,
 } from '../config/motion';
 
+import { CustomButton } from '../components';
 import state from '../store';
 
 const Home = () => {
   const snap = useSnapshot(state); //one current snapshot of the state
-  
+
   return (
-    <div>Home</div>
+    <AnimatePresence>
+      {/* check if we are currently on the homepage */}
+      {snap.intro && (
+        <motion.section 
+          className='home' 
+          {...slideAnimation('left')}
+        >
+          <motion.header
+            {...slideAnimation('down')}
+          >
+            <img 
+              src="./threejs.png"
+              alt="logo"
+              className='w-8 h-8 object-contain'
+            />
+          </motion.header>
+
+          <motion.div
+            className='home-content'
+            {...headContainerAnimation}
+          >
+            <motion.div {...headTextAnimation}>
+              <h1 className='head-text'>
+                BUTTS <br className='xl:block hidden'/> ARE <br /> BIG.
+              </h1>
+            </motion.div>
+
+            <motion.div
+              {...headContentAnimation}
+              className='flex flex-col gap-5'
+            >
+              <p className='max-w-md font-normal text-gray-600 text-base'>
+                Create an exclusive shirt with our cutting edge customization tool. <strong>Unleash your imagination</strong>{' '} and define your own style. I hope there are butts...
+              </p>
+
+              {/* Button */}
+              <CustomButton 
+                type='filled'
+                title='Customize Now'
+                handleClick={() => state.intro = false} //update the state
+                customStyles='w-fit px-4 py-2.5 font-bold text-sm'
+              />
+
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
   )
 }
 
